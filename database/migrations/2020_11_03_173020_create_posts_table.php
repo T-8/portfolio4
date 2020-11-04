5ac59f35c2abb6225e61cfe6ee28171258e94a76
T-8 <sundaradotto@gmail.com>
2020-11-04 14:44:33 +0900

    モデル，コントローラー，マイグレートの生成

---
+++ database/migrations/2020_11_03_173020_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->integer('user_id');
+            $table->string('title', 50);
+            $table->string('text', 255);
+            $table->string('post_img', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
