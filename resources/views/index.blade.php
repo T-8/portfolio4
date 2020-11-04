274044004a7282a01a26befbac5b5c373d59c187
T-8 <sundaradotto@gmail.com>
2020-11-04 15:07:44 +0900

    index画面の作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,23 @@
         </div>
 
         <div class="col-md-8">
+          <a href="{{route('register')}}">
             <div class="card">
-                <div class="card-header">{{ __('記事一覧') }}</div>
+                <div class="card-body">
+                  <div class="title h3">
+                    title
+                  </div>
+                  <div class="float-right">
+                    <div class="icon float-left">
+                      icon
+                    </div>
+                    <div class="name float-left">
+                      名前
+                    </div>
+                  </div>
+                </div>
             </div>
+          </a>
         </div>
     </div>
 </div>
