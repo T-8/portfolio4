0fd37069669eb21442b1c0b46cef818b749f1679
T-8 <sundaradotto@gmail.com>
2020-11-04 15:27:33 +0900

    postコントローラーの編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate(10);
+        return view('index', compact('posts'));
     }
 
     /**
@@ -23,7 +25,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
 
     /**
@@ -34,7 +36,12 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $post = new Post;
+      $post->title = $request->input('title');
+      $post->text = $request->input('text');
+      $post->post_img = $request->input('post_img');
+      $post->save();
+      return redirect('index');
     }
 
     /**
@@ -45,7 +52,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('show', compact('post'));
     }
 
     /**
@@ -56,7 +65,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('edit', compact('post'));
     }
 
     /**
@@ -68,7 +78,12 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $post = Post::find($id);
+      $post->title = $request->input('title');
+      $post->text = $request->input('text');
+      $post->post_img = $request->input('post_img');
+      $post->save();
+      return redirect('index');
     }
 
     /**
@@ -79,6 +94,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $post = Post::find($id);
+      $post->delete();
+      return redirect('index');
     }
 }
