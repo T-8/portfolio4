2066eae881d7d3a0c0e49131c803aa9f74784158
T-8 <sundaradotto@gmail.com>
2020-12-04 18:57:50 +0900

    マイグレーション編集

--- database/migrations/2020_11_03_173020_create_posts_table.php
+++ database/migrations/2020_11_03_173020_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('post_id');
+            $table->bigIncrements('id');
             $table->integer('user_id');
             $table->string('title', 50);
             $table->string('text', 255);
